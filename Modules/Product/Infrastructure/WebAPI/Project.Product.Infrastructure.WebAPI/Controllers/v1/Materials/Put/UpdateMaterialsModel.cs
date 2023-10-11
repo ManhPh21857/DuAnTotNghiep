b059b1ba39542ce685d4c8c241e5d34717865e25
ManhNQ <114049097+ManhPh21857@users.noreply.github.com>
2023-10-11 12:04:02 +0700

    hoan thanh materials

---
+++ Modules/Product/Infrastructure/WebAPI/Project.Product.Infrastructure.WebAPI/Controllers/v1/Materials/Put/UpdateMaterialsModel.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Product.Infrastructure.WebAPI.Controllers.v1.Materials.Put
+{
+    public class UpdateMaterialsModel
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+    }
+    public class UpdateMaterialsModelValidator : AbstractValidator<UpdateMaterialsModel>
+    {
+        public UpdateMaterialsModelValidator()
+        {
+            RuleFor(v => v.Name)
+                .NotEmpty()
+                .WithMessage($"{nameof(UpdateMaterialsModel.Name)} can not be empty");
+
+
+            RuleFor(v => v.Id)
+                .NotEmpty()
+                .WithMessage($"{nameof(UpdateMaterialsModel.Id)} can not be empty");
+
+        }
+    }
+}
