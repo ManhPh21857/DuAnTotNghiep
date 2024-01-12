dcb2d3ac9936b1c4163ba652d6677c022a68783c
ManhNQ <114049097+ManhPh21857@users.noreply.github.com>
2024-01-12 22:31:55 +0700

    new

--- Modules/Sales/ApplicationService/Project.Sales.ApplicationService/SaleCounters/Command/CreateOrderDetailCommandHandler.cs
+++ Modules/Sales/ApplicationService/Project.Sales.ApplicationService/SaleCounters/Command/CreateOrderDetailCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Project.Core.ApplicationService.Commands;
 using Project.Core.Domain;
 using Project.Sales.Domain.SaleCounters;
+using Project.Sales.Domain.Vouchers;
 using Project.Sales.Integration.SaleCounters.Command;
 
 namespace Project.Sales.ApplicationService.SaleCounters.Command
@@ -9,18 +10,34 @@ namespace Project.Sales.ApplicationService.SaleCounters.Command
     {
         private readonly ISaleCounterRepository saleCounterRepository;
         private readonly ISessionInfo sessionInfo;
-        public CreateOrderDetailCommandHandler(ISaleCounterRepository saleCounterRepository, ISessionInfo sessionInfo)
+        private readonly IVoucherRepository voucherRepository;
+        public CreateOrderDetailCommandHandler(ISaleCounterRepository saleCounterRepository, IVoucherRepository voucherRepository, ISessionInfo sessionInfo)
         {
             this.saleCounterRepository = saleCounterRepository;
             this.sessionInfo = sessionInfo;
+            this.voucherRepository = voucherRepository;
         }
 
         public async override Task<CreateOrderDetailCommandResult> Handle(CreateOrderDetailCommand request, CancellationToken cancellationToken)
         {
             var code = Guid.NewGuid();
+
+            
+                var voucher = await this.voucherRepository.GetVoucher(request.Order.VoucherId);
+                if (voucher is null)
+                {
+                    throw new DomainException("", "Voucher không tồn tại");
+                }
+
+                if (voucher.Quantity < 1)
+                {
+                    throw new DomainException("", "Voucher đã hết số lần sử dụng");
+                }
+
+                await this.voucherRepository.UpdateVoucherQuantity(request.Order.VoucherId, voucher.Quantity - 1);
+            
             var order = new OrderInfo
             {
-
                 EmployeeId = this.sessionInfo.UserId.value,
                 OrderCode = code,
                 Address = "Tại Quầy",
