d419377fe641038f5488730e5b555add82f317af
ManhNQ <114049097+ManhPh21857@users.noreply.github.com>
2024-01-14 08:06:05 +0700

    new

--- Modules/Sales/ApplicationService/Project.Sales.ApplicationService/SaleCounters/Command/CreateOrderDetailCommandHandler.cs
+++ Modules/Sales/ApplicationService/Project.Sales.ApplicationService/SaleCounters/Command/CreateOrderDetailCommandHandler.cs
@@ -24,15 +24,7 @@ namespace Project.Sales.ApplicationService.SaleCounters.Command
 
             
                 var voucher = await this.voucherRepository.GetVoucher(request.Order.VoucherId);
-                if (voucher is null)
-                {
-                    throw new DomainException("", "Voucher không tồn tại");
-                }
-
-                if (voucher.Quantity < 1)
-                {
-                    throw new DomainException("", "Voucher đã hết số lần sử dụng");
-                }
+              
 
                 await this.voucherRepository.UpdateVoucherQuantity(request.Order.VoucherId, voucher.Quantity - 1);
             
